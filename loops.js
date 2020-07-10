ec61c010e6d090f4245bd1d77276c587c412cdcd
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-07-10 21:23:50 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,11 @@
-var array = []
+
 /*TODO: Define a function called doWhileLoop in loops.js. The function should take an integer as an argument.
  Use the incrementVariable() function (you can copy it from this README) in the condition, and console log "I run once regardless."
  while incrementVariable() returns a number less than the parameter received.
  (Your condition might look something like incrementVariable() < num.)
  Remember that it should also console log when receiving 0 as a parameter because the do-while runs before the condition is checked.*/
 function forLoop (array) {
+  array = []
   for (let i = 0; i < 25; i++) {
       array.push (`I am ${i} strange loop${i===1  ? '' : 's'}.`);
   }
